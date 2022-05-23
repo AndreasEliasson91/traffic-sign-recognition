53240f7ac9e21d51862dbad976bc3222c20115ba
Andreas Eliasson <andreaseliasson91@gmail.com>
2022-05-23 16:28:26 +0200

    added initial presentation notebook and moved functions from file_input to other parts of the project

--- application/src/utils/presentation.py
+++ application/src/utils/presentation.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
-import numpy as np
+import io
+
+from PIL import Image
 
 
 def get_relevant_scores(labels: list, scores: list, boxes: list) -> tuple[list, list, list]:
@@ -46,3 +48,25 @@ def show(image, labels, boxes) -> None:
 
         plt.text(x_min, y_min - 10, str(labels[i]), fontsize=6, color='r')
     plt.show()
+
+
+def convert_upload(upload):
+    file = upload
+    file_name = str(list(file.keys())[0])
+    bytes_image = file[file_name]['content']
+    PIL_image = Image.open(io.BytesIO(bytes_image))
+    return PIL_image
+
+
+def print_result(labels: list, scores=None) -> None:
+    """
+    Print the labels and, if we predict a random image, the accuracy score.
+    Only prints the signs that with more than 60% accuracy
+    :param labels: list; list with the predicted or actual signs
+    :param scores: list[int]; list with accuracy scores, only when predicted. None by default
+    :return: None
+    """
+    if scores:
+        [print(f'Sign: {label} - Accuracy: {scores[i]}%') for i, label in enumerate(labels)]
+    else:
+        [print(f'Sign: {label}') for label in labels]
